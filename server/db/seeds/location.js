dde8f53772001416a7dfdd7b965a1f1790a9caae
rich-hape <richard.hape@devacademy.co.nz>
2023-08-29 10:02:31 +1200

    return back to original code on files

--- server/db/seeds/location.js
+++
@@ -1,8 +0,0 @@
-export const seed = (knex) =>
-  knex('location')
-    .del()
-    .then(() =>
-      knex('location').insert([
-        { id: 99901, description: 'Bright Tree Village' },
-      ])
-    )
