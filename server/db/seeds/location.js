7ad4563890c75df43a050db59fddf64c62958d58
rich-hape <richard.hape@devacademy.co.nz>
2023-08-29 09:52:30 +1200

    make changes to location.js file

--- server/db/seeds/location.js
+++ server/db/seeds/location.js
@@ -4,11 +4,5 @@ export const seed = (knex) =>
     .then(() =>
       knex('location').insert([
         { id: 99901, description: 'Bright Tree Village' },
-        { id: 99902, description: 'handy' },
-        { id: 99903, description: 'french' },
-        { id: 99904, description: 'lazy' },
-        { id: 99905, description: 'clever' },
-        { id: 99906, description: 'sporty' },
-        { id: 99907, description: 'bossy' },
       ])
     )
